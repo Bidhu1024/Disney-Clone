67769a79ba55131f94b2797f258c64a60f141ca2
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-10-20 09:17:19 +0530

    UI done firebase will be added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import './App.css';
-import Login from './components/Login';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-    <Router>
-<Routes>
-<Route exact path='/' element={<Login/>}>
-
-</Route>
-</Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App; 
-
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+import Logo1 from "../Assets/Logo1.svg";
+import HomeLogo from '../Assets/Home-icon.svg'
+import Search from '../Assets/Search.svg'
+import WatchList from '../Assets/WatchList.svg'
+import Originals from '../Assets/Original.svg'
+import Series from '../Assets/Series.svg'
+import Movies from '../Assets/Movies.svg'
+const Header = (props) => {
+  return (
+    <Nav>
+      <img
+        src={Logo1}
+        alt="disney"
+        width="80px"
+        max-height="70px"
+        font-size="0"
+      />
+      <NavMenu>
+        <a href="/home">
+            <img src={HomeLogo} alt="a" />
+            <span>HOME</span>
+       </a>
+       <a href="/home">
+            <img src={Search} alt="a" />
+            <span>SEARCH</span>
+       </a>
+       <a href="/home">
+            <img src={WatchList} alt="a" />
+            <span>WATCHLIST</span>
+       </a>
+       <a href="/home">
+            <img src={Originals} alt="a" />
+            <span>ORIGINALS</span>
+       </a>
+       <a href="/home">
+            <img src={Movies} alt="a" />
+            <span>MOVIES</span>
+       </a>
+       <a href="/home">
+            <img src={Series} alt="a" />
+            <span>SERIES</span>
+       </a>
+
+      </NavMenu>
+      <Login>Login</Login>
+    </Nav>
+  );
+};
+export default Header;
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2.1rem;
+  letter-spacing: 1rem;
+  z-index: 3;
+`;
+const NavMenu = styled.div`
+display: flex;
+align-items: center;
+flex-flow: row nowrap;
+height: 100%;
+justify-content: flex-end;
+margin: 0;
+padding: 0;
+position: relative;
+margin-right: auto;
+margin-left: 25px;
+a{
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    
+    img{
+        height: 20px;
+        min-width: 20px;
+        width: 20px;
+        z-index: auto;
+    }
+    span{
+        color: rgb(249,249,249);
+        font-size: 13px;
+        letter-spacing: 1.4px;
+        line-height: 1.08px;
+        padding: 2px 0;
+        white-space: nowrap;
+        position: relative;
+    
+    &:before{
+       
+        
+        background-color: rgb(249,249,249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: " ";
+        opacity: 0;
+        height: 3px;
+        position: absolute;
+        right: 0px;
+        left: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition:all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+
+    }
+
+    }
+
+    &:hover{
+    span:before{
+        transform: scaleX(1);
+        visibility: visible;
+        opacity:1 !important;
+    }
+
+    }
+}
+
+`;
+const Login = styled.a`
+background-color: rgba(0,0,0,0.6);
+padding: 8px 16px;
+text-transform: uppercase;
+letter-spacing: 1.4px;
+border: 1px solid white;
+border-radius: 4px;
+transition: all .2s ease-out;
+
+&:hover{
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+}
+`;
